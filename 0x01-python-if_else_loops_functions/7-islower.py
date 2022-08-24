6cf0198fa01fe6ad83b4c7b4e246be7a00f085d5
Hika7 <hikadaka1@gmail.com>
2022-08-24 12:58:52 -0700

    correct

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,6 +3,6 @@ def islower(c):
     for s in range(97, 123):
         if (ord(c) == s):
             return True
-else:
-    continue
-return False
+        else:
+            continue
+        return False
