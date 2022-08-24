3dca701fd560143fe2ee872d664f677daba35ff7
Hika7 <hikadaka1@gmail.com>
2022-08-24 12:53:20 -0700

    correct

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for x in range(9):
     for y in range(1, 10):
         if (y < x or y == x):
             continue
-if (x != 8):
-    print("{:d}{:d}".format(x, y), end=', ')
-else:
-    print("{:d}{:d}".format(x, y))
+        if (x != 8):
+            print("{:d}{:d}".format(x, y), end=', ')
+        else:
+            print("{:d}{:d}".format(x, y))
