a831e2a1fd6d49aea5ed7dc6b3b9ef6a4a140c5c
Hika7 <hikadaka1@gmail.com>
2022-09-13 13:02:30 -0700

    correction

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-'''I am creating an empty class with square in it''' 
+'''I am creating an empty class with square in it'''
+'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
 class Square:
     pass
