774d89280579124609bf9500e3a440d269d7baf1
Hika7 <hikadaka1@gmail.com>
2022-09-13 13:31:45 -0700

    correction

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+#python3 -c 'print(__import__("my_module").MyClass.__doc__)'
 
 '''Empty square.'''
-
-'''
-python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-'''
 
 
 class Square: 
