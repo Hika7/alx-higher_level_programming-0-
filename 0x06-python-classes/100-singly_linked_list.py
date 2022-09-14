1156b6d028704b0e5680a3f4b2302885e393b8d7
Hika7 <hikadaka1@gmail.com>
2022-09-14 15:00:51 -0700

    all tasks

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -38,6 +38,7 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
+
 class SinglyLinkedList:
     """Represent a singly-linked list."""
 
@@ -55,19 +56,19 @@ class SinglyLinkedList:
             value (Node): The new Node to insert.
         """
         new = Node(value)
-    if self.__head is None:
-        new.next_node = None
-        self.__head = new
-    elif self.__head.data > value:
-        new.next_node = self.__head
-        self.__head = new
-    else:
-        tmp = self.__head
-        while (tmp.next_node is not None and
-                tmp.next_node.data < value):
-            tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+                new.next_node = tmp.next_node
+                tmp.next_node = new
 
     def __str__(self):
         """Define the print() representation of a SinglyLinkedList."""
