cd4a66099ce6ec8659144ee2d1ce2eb497f0e514
Hika7 <hikadaka1@gmail.com>
2022-09-14 14:40:36 -0700

    all tasks

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -10,30 +10,30 @@ class Square:
         """ Initialize attributes"""
         self.size = size
 
-        @property
-        def size(self):
-            """ gets the size"""
-            return self.__size
+    @property
+    def size(self):
+        """ gets the size"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """ sets the size with safe Assignment"""
-            if type(value) is not int:
-                raise TypeError('size must be an integer')
-            if (value < 0):
-                raise ValueError('size must be >= 0')
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """ sets the size with safe Assignment"""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if (value < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
-        def area(self):
-            """ Return the area of the square"""
-            return (self.__size * self.__size)
+    def area(self):
+        """ Return the area of the square"""
+        return (self.__size * self.__size)
 
-        def my_print(self):
-            """ prints in stdout the square with the character # """
-            if (self.size != 0):
-                for x in range(self.__size):
-                    for y in range(self.__size):
-                        print("#", end='')
-                        print('')
-                    else:
-                        print('')
+    def my_print(self):
+        """ prints in stdout the square with the character # """
+        if (self.size != 0):
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print("#", end='')
+                    print('')
+                else:
+                    print('')
