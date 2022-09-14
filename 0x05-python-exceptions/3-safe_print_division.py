423c0c2ace1f3d3dd6608df9a018d0ee72f20883
Hika7 <hikadaka1@gmail.com>
2022-09-14 13:24:58 -0700

    correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     ir = 0
     try:
-    ir = (a / b)
+        ir = (a / b)
     except:
         ir = None
     finally:
         print("Inside result: {}".format(ir))
-        return (ir)
+    return (ir)
